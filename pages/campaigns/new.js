a0246e09001c21704331918446925884f54b64ca
YarkoL <yarko.dev@gmail.com>
2018-08-28 23:26:48 +0300

    campaign creation page

---
+++ pages/campaigns/new.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+import { Button, Form, Input, Message } from 'semantic-ui-react'
+import factory from '../../ethcode/factory'
+import web3 from '../../ethcode/web3'
+
+class CampaignNew extends Component {
+	state = {
+		minimumContribution: '',
+		errorMsg: ''
+	};
+
+	onSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const accounts = await web3.eth.getAccounts();
+			await factory.methods
+			.createCampaign(this.state.minimumContribution)
+			.send({
+				from: accounts[0]
+			});	
+		} catch(err) {
+			this.setState({errorMsg:err.message});
+		}
+	};
+
+	render() {
+		return (
+			<Layout>
+				<h3>Create a campaign</h3>
+				<Form 
+					onSubmit={this.onSubmit} 
+					error={!!this.state.errorMsg}
+				>
+					<Form.Field>
+						<label>Minimum contribution</label>
+						<Input 
+							label="Wei" 
+							labelPosition="right"
+							value = {this.state.minimumContribution}
+							onChange = {
+								event => this.setState({
+									minimumContribution: event.target.value
+								})
+							}
+						>
+						</Input>
+					</Form.Field>
+					<Message error header="Oops!" 
+						content={this.state.errorMsg}/>
+					<Button primary type='submit'>Create!</Button>
+				</Form>
+				
+			</Layout>
+		); 
+	}
+}
+
+export default CampaignNew;
